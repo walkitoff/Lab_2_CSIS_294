acbfa67de4267612f69d0b7522dd3c28620049fb
walkitoff <tylerad83@gmail.com>
2022-08-30 22:24:38 -0700

    added: Classes: MerkleManager, MerkleThread with TODO: notes

---
+++ src/MerkleManager.java
@@ -0,0 +1,28 @@
+public class MerkleManager {
+
+    //DONE: Instance Variables
+    public static volatile String userWord; //user's inputted word
+    public static String expected_Merkle_Root;
+    public static String merkleRoot = null;
+    public static int strikes = 0;
+
+    //TODO: create public method: manage()  to be called in main() of MerkleManager_Test
+        //see READEME.md ii. 2. for full instructions
+    public void manage() {
+        //TODO: prompt user for Expected Merkle Root  ...aka the hash of the merkle root
+        //TODO: put user input into expected_Merkle_Root variable
+        //TODO: Start 3 separate threads: instantiate and start() new threads for ...
+        // MerkleThread, RogueThread and MonitorThread
+        //TODO: Begin UI question Loop:
+             //while(true)...ask user for a word and put it into  String userWord;
+    }
+
+    //TODO: create method: public static synchronized String grabWord()
+    public static synchronized String grabWord() {
+        //TODO: Puts instance variable of user's word into a temp String variable then
+        // sets instance variable userWord to null;
+        //returns temp String variable
+    }
+
+
+}
