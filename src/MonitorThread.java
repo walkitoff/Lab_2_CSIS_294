af42aae2bf615dab2c593741e09f37d0257a5496
renorris <reese.norris@gmail.com>
2022-09-05 12:05:57 -0700

    cleanup, add sleep to loop

--- src/MonitorThread.java
+++ src/MonitorThread.java
@@ -1,12 +1,16 @@
-// Authors: Tyler Dollick
+// Monitor Thread
+// Authors: Tyler Dollick, Reese Norris
+
 public class MonitorThread implements Runnable {
+
     @Override
     public void run() {
-        while(true) {
-            if (MerkleManager.actualMerkleRoot != null ) {
-                if(MerkleManager.expectedMerkleRoot.equals(MerkleManager.actualMerkleRoot)) {
+        while (true) {
+            if (MerkleManager.actualMerkleRoot != null) {
+                if (MerkleManager.expectedMerkleRoot.equals(MerkleManager.actualMerkleRoot)) {
                     System.out.println("**** YOU WIN **** " + MerkleManager.actualMerkleRoot);
-                }else {
+                }
+                else {
                     System.out.println("**** YOU LOSE **** ");
                     System.out.println("      You entered: " + MerkleManager.expectedMerkleRoot);
                     System.out.println("Correct answer is: " + MerkleManager.actualMerkleRoot);
@@ -14,12 +18,14 @@ public class MonitorThread implements Runnable {
                 System.out.println("\nExiting app...");
                 System.exit(0);
             }
-            if(MerkleManager.strikeCount == 3){
+            if (MerkleManager.strikeCount == 3) {
                 System.out.println("*** THREE STRIKES: *** YOU'RE OUT! ***");
                 System.out.println("\nExiting app...");
                 System.exit(0);
             }
+
+            Util util = new Util();
+            util.sleep(1);
         }
     }
 }
-
